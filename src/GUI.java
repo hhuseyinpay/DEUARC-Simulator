df4a8df7e3421df670951db5272c65a96c56c0b5
Hasan Hüseyin Pay <hhuseyinpay@gmail.com>
2017-04-29 17:50:18 +0300

    initial

---
+++ src/GUI.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+/**
+ * Created by hhp on 29/04/2017.
+ */
+public class GUI {
+    public JPanel panel1;
+    private JButton button1;
+}
---
+++ src/hello.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+public class hello {
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MyForm");
+        frame.setContentPane(new GUI().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
